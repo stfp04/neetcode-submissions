623a356bba712429bbf86a32ddf07a1b29bf4ca0
stfp04 <rafael.augusto.garcia@tecnico.ulisboa.pt>
2026-05-18 18:58:45 +0100

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.py
@@ -0,0 +1,38 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        L, length = 0, 0
+        maxChar, maxCount = 0, 0
+        toReplace = 0
+        letters = {}
+
+        for R in range(len(s)):
+            print(f"Letter: {s[R]} Position: {R}")
+
+            if s[R] not in letters:
+                letters[s[R]] = 0
+            letters[s[R]] += 1
+
+
+            if letters[s[R]] > maxCount:
+                maxChar = s[R]
+                maxCount = letters[s[R]]
+                
+            toReplace = (R - L + 1) - maxCount
+            print(f"To replace: {toReplace}")
+
+            while toReplace > k:
+                letters[s[L]] -= 1
+                if s[L] == maxChar:
+                    maxChar, maxCount = max(letters.items(), key=lambda x: x[1])
+                    toReplace = (R - L) - maxCount
+                toReplace = (R - L) - maxCount
+
+                if letters[s[L]] == 0:
+                    del letters[s[L]]
+
+                L += 1
+
+
+            length = max(length, R - L + 1)
+
+        return length
